d1e29ecd74e080ff5d2f72935f32e1ad2328d5f9
Jpritchett09 <Jpritch21@live.com>
2023-09-22 15:37:42 -0500

    Addition of OOP for user.py

---
+++ user.py
@@ -0,0 +1,20 @@
+from mysqlconnection import connectToMySQL
+
+class User: 
+    def __init__(self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.role = data['role']
+        self.email = data['email']
+        self.username = data['username']
+        self.created_at = data['created-at']
+        self.updated_at = data['updated-at']
+    @classmethod
+    def get_all_users(cls):
+        query = "SELECT * FROM user;"
+        results = connectToMySQL('crm_db').query_db(query)
+        users = []
+        for user in results:
+            users.append(cls(user))
+        return users
