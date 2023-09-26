611f9b987259dbe7a3c00e6593971b024db65d17
Jacob Pritchett <117482972+Jacob-Pritchett1@users.noreply.github.com>
2023-09-25 19:19:08 -0500

    Create company.py
    Added a company.py which is the model for Company. Connected it to DB and created 1 method.

---
+++ company.py
@@ -0,0 +1,19 @@
+from mysqlconnection import connectToMySQL
+
+class Company:
+  def __init__(self, data):
+    self.id = data['id']
+    self.company_nmae = data['company_name']
+    self.phone_number = data['phone_number']
+    self.point_of_contact = data['point_of_contact']
+    self.created_at = data['created_at']
+    self.updated_at = data['updated_at']
+    self.user = None
+  @classmethod
+  def get_all_companies(cls):
+    query = 'SELECT * FROM company;'
+    results = connectToMySQL('crm_db').query_db(query)
+    companies = []
+    for company is results:
+      companies.append(cls(user))
+    return companies
